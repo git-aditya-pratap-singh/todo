5f9b9bad91a795427c42c8e04082aef40cda5ccc
Aditya Pratap Singh <68802755+Adityasingh39@users.noreply.github.com>
2023-02-10 19:47:12 +0530

    todoapp

---
+++ todoapp-redux/actions/index.js
@@ -0,0 +1,18 @@
+export const addTodo = (data) =>{
+    return{
+        type: "ADD_TODO",
+        payload : {
+            id : new Date().getTime().toString(),
+            data : data
+        }
+    }
+}
+
+export const deleteTodo = (id) =>{
+    return{
+        type: "DELETE_TODO",
+        payload: id
+    }
+}
+
+
